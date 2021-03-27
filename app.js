e04718c23f430f92024f49190b65b8a5920a8def
eggplantiny <eggplantiny@gmail.com>
2021-03-27 17:00:10 +0900

    todo 추가 및 삭제

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
-const todoList = []
-
 const todoInput = document.getElementById('todo-input')
 const todoInputBtn = document.getElementById('todo-input-button')
+const listEl = document.getElementById('list')
 
 todoInputBtn.addEventListener('click', function () {
     const text = todoInput.value
+    const li = document.createElement('li')
 
-    window.alert(text)
+    li.innerText = text
+    todoInput.value = ''
+
+    li.addEventListener('click', function () {
+        listEl.removeChild(li)
+    })
+
+    listEl.appendChild(li)
 })
 
 todoInput.addEventListener('keydown', function (event) {
